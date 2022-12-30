c887fc5fcbc8e66f20bcf40eaa1050425df22abd
datisekai <datly030102@gmail.com>
2022-12-30 14:22:14 +0700

    remove cart, order, login, signup --  add section in home

--- components/sections/Section5.tsx
+++ components/sections/Section5.tsx
@@ -1,8 +1,7 @@
-import React, { FC } from "react";
+import React from "react";
 import { BsChevronDown } from "react-icons/bs";
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import HomeCard from "../cards/HomeCard";
-
 const data = [
   "/images/1.jpg",
   "/images/2.jpg",
@@ -10,37 +9,25 @@ const data = [
   "/images/4.jpg",
   "/images/1.jpg",
   "/images/2.jpg",
-  "/images/3.jpg",
-  "/images/4.jpg",
 ];
 
-interface Section3Props {
-  title: string;
-}
-
-const Section3: FC<Section3Props> = ({ title }) => {
+const Section5 = () => {
   return (
     <div className="mt-4 py-4 max-w-[1200px] w-[calc(100%-16px)] mx-auto">
       <div className="flex justify-between items-center py-1 border-b-[2px]">
         <h1 className="inline font-bold text-lg md:text-xl border-line uppercase">
-          {title}
+          Trang sức cưới
         </h1>
         <div className="flex items-center text-sm md:text-md hover:text-primary transition-all hover:cursor-pointer">
           <button>Xem tất cả</button>
           <BsChevronDown className="ml-1" />
         </div>
       </div>
-      <div className="mt-4 flex">
-        <div className="w-[180px] hidden md:flex flex-col ">
-          <div>
-            {["Kim cương viên", "Nhẫn", "Hoa tai", "Mặt dây"].map((item) => (
-              <div key={item} className="py-2 hover:text-primary transition-all cursor-pointer border-b-[2px]">
-                <span>{item} (2)</span>
-              </div>
-            ))}
-          </div>
+      <div className="mt-4 flex  flex-col md:flex-row">
+        <div className="w-full md:w-[40%]">
+          <LazyLoadImage src="/images/section5.png" />
         </div>
-        <div className="flex-1 md:ml-4 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-4 gap-x-2 gap-y-8">
+        <div className="flex-1 mt-2 md:mt-0 md:ml-4 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-3 gap-x-2 gap-y-8">
           {data.map((item) => (
             <HomeCard data={item} key={item} />
           ))}
@@ -50,4 +37,4 @@ const Section3: FC<Section3Props> = ({ title }) => {
   );
 };
 
-export default Section3;
+export default Section5;
---
+++ components/sections/Section6.tsx
@@ -0,0 +1,60 @@
+import Link from "next/link";
+import React from "react";
+import { BsChevronDown } from "react-icons/bs";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+const data = [
+  "/images/section6-1.png",
+  "/images/section6-2.png",
+  "/images/section6-1.png",
+];
+
+const Section6 = () => {
+  return (
+    <div className="max-w-[1200px] mx-auto mt-2 px-2">
+      <div className="flex justify-between items-center py-1 border-b-[2px]">
+        <h1 className="inline font-bold text-lg md:text-xl border-line uppercase">
+          Bộ sưu tập
+        </h1>
+        <div className="flex items-center text-sm md:text-md hover:text-primary transition-all hover:cursor-pointer">
+          <button>Xem tất cả</button>
+          <BsChevronDown className="ml-1" />
+        </div>
+      </div>
+      <div className="mt-4">
+        <Swiper
+          modules={[Navigation, Pagination]}
+          spaceBetween={10}
+          breakpoints={{
+            768: {
+              slidesPerView: 2,
+            },
+          }}
+          navigation
+          color="#EA8143"
+        >
+          {data.map((item) => (
+            <SwiperSlide key={item}>
+              <div className="flex flex-col items-center">
+                <LazyLoadImage src={item} className="w-full rounded-lg " />
+                <div className="mt-2 flex flex-col">
+                  <h4 className="uppercase font-bold">BST Nhẫn đính hôn</h4>
+                  <Link href={`/search?keyword=${encodeURI("BST Nhẫn đính hôn")}`}>
+                    <button className="bg-[#D9D9D9] w-full text-[#898989] px-4 hover:bg-primary hover:text-white transition-all py-1 mt-2">
+                      Xem chi tiết
+                    </button>
+                  </Link>
+                </div>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+    </div>
+  );
+};
+
+export default Section6;
