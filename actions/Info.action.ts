456a06d25446f04350913836dd607a49d883f5bb
thuyngocmaithy <thuyngocmaithyy@gmail.com>
2023-01-17 20:23:02 +0700

    test

---
+++ actions/Info.action.ts
@@ -0,0 +1,26 @@
+import axiosClient from "../config/axiosClient";
+
+const InfoAction = {
+  getAll: async () => {
+    try {
+      const result = await axiosClient.get("/info");
+      return result.data;
+    } catch (error) {        
+      console.log(error);
+    }
+  },
+  add: async (data: any) => {
+    const result = await axiosClient.post("/info", data);
+    return result.data;
+  },
+  update: async (data: any) => {
+    const result = await axiosClient.put(`/info/${data.id}`, data);
+    return result.data;
+  },
+  delete: async (id: string | number) => {
+    const result = await axiosClient.delete(`/info/${id}`);
+    return result.data;
+  },
+};
+
+export default InfoAction;
---
+++ models/Info.model.ts
@@ -0,0 +1,7 @@
+
+export interface InfoModel {
+    id: number;
+    title: string;
+    content: string;
+    image: string;
+}
