5a8bedc670b0e6d67e3a7719be5282544858d3a6
datisekai <datly030102@gmail.com>
2023-02-28 16:50:09 +0700

    fix redirect admin

--- middleware.ts
+++ middleware.ts
@@ -4,15 +4,11 @@ import type { NextRequest } from "next/server";
 // This function can be marked `async` if using `await` inside
 function middleware(request: NextRequest) {
   const token = request.cookies.get("token");
-  const detailActions = JSON.parse(request.cookies.get('detailActions')?.value || "[]");
 
   if (!token) {
     return NextResponse.redirect(new URL("/login", request.url));
   }
 
-  if(detailActions?.length === 0){
-    return NextResponse.redirect(new URL("/", request.url));
-  }
   return NextResponse.next();
 }
 
